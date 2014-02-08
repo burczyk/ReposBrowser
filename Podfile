92208a8809dd7480bd1390bc1130c361e6fec04a
Kamil Burczyk <kamil.burczyk@sigmapoint.pl>
2014-02-08 21:58:27 +0100

    Initial commit

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, "6.0"
+
+target "ReposBrowser" do
+
+pod 'NetworkLib', :path => '../NetworkLib'
+
+end
+
+target "ReposBrowserTests" do
+
+end
+
--- ReposBrowser/ViewController.m
+++ ReposBrowser/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "NetworkLib.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
 
@@ -17,13 +21,17 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)showRepos:(UIButton *)sender {
+    NetworkLib *nl = [[NetworkLib alloc] init];
+    [nl getGithubReposForUser:_textField.text withSuccess:^(id responseObject) {
+        _textView.text = [NSString stringWithFormat:@"%@", responseObject];
+    } failure:^(NSError *error) {
+        _textView.text = @"Error occured";
+    }];
+    
+    [_textField resignFirstResponder];
 }
 
 @end
