78ef77bd82186984f9d3c91196b727bb0e022a95
Kamil Burczyk <kamil.burczyk@sigmapoint.pl>
2014-02-08 18:35:32 +0100

    Initial Commit

---
+++ ReposBrowser/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ReposBrowser
+//
+//  Created by Kamil Burczyk on 08.02.2014.
+//  Copyright (c) 2014 Sigmapoint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ReposBrowser/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ReposBrowser
+//
+//  Created by Kamil Burczyk on 08.02.2014.
+//  Copyright (c) 2014 Sigmapoint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
